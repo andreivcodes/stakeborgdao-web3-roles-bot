5ef1e2d6544d61fd51a808df52220b2e77bee76f
Andrei Voinea <8058187+andreivdev@users.noreply.github.com>
2022-04-15 12:15:48 +0300

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,10 +2,7 @@ import { NextResponse } from "next/server";
 
 export function middleware(req, ev) {
   const currentEnv = process.env.NODE_ENV;
-  const proto =
-    req.headers["x-forwarded-proto"] || req.connection.encrypted
-      ? "https"
-      : "http";
+  const proto = req.headers["x-forwarded-proto"] ? "https" : "http";
   if (currentEnv === "production" && proto != "https") {
     return NextResponse.redirect(`https://roles.stakeborgdao.xyz/`, 301);
   }
