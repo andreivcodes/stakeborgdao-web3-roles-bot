ccf8eeb911817d2563c205e6151df94817313f6c
Andrei Voinea <8058187+andreivdev@users.noreply.github.com>
2022-04-15 12:21:48 +0300

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(req, ev) {
-  const currentEnv = process.env.NODE_ENV;
-  if (
-    currentEnv === "production" &&
-    req.headers["x-forwarded-proto"] !== "https"
-  ) {
-    return NextResponse.redirect(`https://roles.stakeborgdao.xyz/`, 301);
-  }
-  return NextResponse.next();
-}
+// export function middleware(req, ev) {
+//   const currentEnv = process.env.NODE_ENV;
+//   if (
+//     currentEnv === "production" &&
+//     req.headers["x-forwarded-proto"] !== "https"
+//   ) {
+//     return NextResponse.redirect(`https://roles.stakeborgdao.xyz/`, 301);
+//   }
+//   return NextResponse.next();
+// }
