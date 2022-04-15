22bc8242e7a663828d851771c685318f5617da24
Andrei Voinea <8058187+andreivdev@users.noreply.github.com>
2022-04-15 12:07:30 +0300

    Create _middleware.js

---
+++ pages/_middleware.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req, ev) {
+  const currentEnv = process.env.NODE_ENV;
+
+  if (
+    currentEnv === "production" &&
+    req.headers.get("x-forwarded-proto") !== "https"
+  ) {
+    return NextResponse.redirect(
+      `https://roles.stakeborgdao.xyz/${req.nextUrl.pathname}`,
+      301
+    );
+  }
+  return NextResponse.next();
+}
